cec4337cc9ad5777f66ebfac9dbab7680266c9c7
Obaid-Rehman <obaidkhattak@gmail.com>
2021-08-10 14:53:41 +0500

    Set up bundling files for Operator-SDK registration

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -21,13 +21,14 @@ import (
 	"path/filepath"
 	"testing"
 
-	ctrl "sigs.k8s.io/controller-runtime"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/apimachinery/pkg/types"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -41,6 +42,7 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
+var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
@@ -61,34 +63,43 @@ var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
+	t := true
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
+		UseExistingCluster:    &t,
 	}
 
-	cfg, err := testEnv.Start()
+	var err error
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = apicodegenv1beta1.AddToScheme(scheme.Scheme)
+	err = clientgoscheme.AddToScheme(clientgoscheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = apicodegenv1beta1.AddToScheme(clientgoscheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
-
 	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme.Scheme,
+		Scheme: clientgoscheme.Scheme,
 	})
 	Expect(err).ToNot(HaveOccurred())
 
 	err = (&APIMaticReconciler{
 		Client: k8sManager.GetClient(),
-		Scheme: k8sClient.Scheme(),
+		Scheme: k8sManager.GetScheme(),
 	}).SetupWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	k8sClient = k8sManager.GetClient()
+	Expect(k8sClient).ToNot(BeNil())
 
 	configMap := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
@@ -100,20 +111,20 @@ var _ = BeforeSuite(func() {
 			Namespace: ConfigMapNamespace,
 		},
 		Data: map[string]string{
-			"License.lic": "\u003cLicense\u003e\r\n  \u003cId\u003e2c5a837e-ca01-447a-a393-d509899c7662\u003c/Id\u003e\r\n  \u003cType\u003eStandard\u003c/Type\u003e\r\n  \u003cExpiration\u003eMon, 04 Aug 2025 00:00:00 GMT\u003c/Expiration\u003e\r\n  \u003cQuantity\u003e2\u003c/Quantity\u003e\r\n  \u003cProductFeatures\u003e\r\n    \u003cFeature name=\"importFormats\"\u003eAPIBluePrint,APIElements,Postman2,Wsdl,WADL2009,WADL2006,Raml,Raml10,Swagger20,SwaggerYaml,Swagger10,OpenApi3Json,OpenApi3Yaml,IODocs,IODocsV0314,Insomnia,InsomniaYaml,RAMLAsJson,GoogleDiscovery,Postman\u003c/Feature\u003e\r\n    \u003cFeature name=\"exportFormats\"\u003eSwagger20,SwaggerYaml,Swagger10,OpenApi3Json,OpenApi3Yaml,Wsdl,APIBluePrint,APIElements,Postman2,WADL2009,WADL2006,WSDL,IODocs,IODocsV0314,RAML,RAML10,GoogleDiscovery,Postman20,Insomnia,InsomniaYaml\u003c/Feature\u003e\r\n    \u003cFeature name=\"platforms\"\u003edocs,java,dotnet,ios,node,ruby,python,go,android,php\u003c/Feature\u003e\r\n  \u003c/ProductFeatures\u003e\r\n  \u003cCustomer\u003e\r\n    \u003cName\u003eDeveloper\u003c/Name\u003e\r\n    \u003cEmail\u003edeveloper@apimatic.io\u003c/Email\u003e\r\n  \u003c/Customer\u003e\r\n  \u003cSignature\u003eMEUCIQDrxYeTDrouY4FWz0mgHu+Um6d5UzDNBkwpnZr3MHDkBQIgCgodCwLQVWQC7T7DVRAWWMSMfH6LU3t0XERBQv7FyfI=\u003c/Signature\u003e\r\n\u003c/License\u003e",
+			"License.lic": "<Add the License.lic files contents here>",
 		},
 	}
 
 	Expect(k8sClient.Create(context.Background(), configMap)).Should(Succeed())
-
-	go func() {
-		err = k8sManager.Start(ctrl.SetupSignalHandler())
-		Expect(err).ToNot(HaveOccurred())
-	}()
 }, 60)
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
+
+	configMap := &corev1.ConfigMap{}
+	Expect(k8sClient.Get(context.Background(), types.NamespacedName{Name: ConfigMapName, Namespace: ConfigMapNamespace}, configMap)).Should(Succeed())
+	Expect(k8sClient.Delete(context.Background(), configMap)).Should(Succeed())
+
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.16
 
 require (
 	github.com/banzaicloud/k8s-objectmatcher v1.5.1
-	github.com/go-logr/logr v0.3.0
-	github.com/onsi/ginkgo v1.14.1
-	github.com/onsi/gomega v1.10.2
-	k8s.io/api v0.20.2
-	k8s.io/apimachinery v0.20.2
-	k8s.io/client-go v0.20.2
-	sigs.k8s.io/controller-runtime v0.8.3
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.13.0
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/tools v0.1.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	k8s.io/api v0.21.1
+	k8s.io/apimachinery v0.21.1
+	k8s.io/client-go v0.21.1
+	sigs.k8s.io/controller-runtime v0.9.0
 )
